0e0b26723895674c196ed6cf818e2d4528e5b885
MinMax0 <34135592+MinMax0@users.noreply.github.com>
2018-04-10 00:33:57 +0530

    Add files via upload

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/d6e3022e54bc21b6b1bdc0a41d974637.js');
+importScripts('https://cdn.pushcrew.com/sw/f37df7bb3835851be2e88b9fa61f2c05.js');
